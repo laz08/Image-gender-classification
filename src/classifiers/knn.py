10fbe41ed51cedbf6608c2690f0a0d5846c98bd7
Laura <lazeru08@gmail.com>
2018-05-13 18:48:22 +0200

    Plotting histograms WIP

--- src/classifiers/knn.py
+++ src/classifiers/knn.py
@@ -1,10 +1,40 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import sys, getopt
-import math
-
+import sys, getopt, os
+import math, cv2
+from matplotlib import pyplot as plot
 import Utils
+
+_PATH_TO_PHOTOS = "../../datasets/facesInTheWild/"
+
+def getImageFeatures(img, size = (150, 150)):
+    "Resizes to 150x150 and Flatten: List of raw pixel intensities"
+    return cv2.resize(img, size).flatten()
+
+
+def plotHistogram(hist):
+    "Plots histogram for debugging"
+    plot.figure()
+    plot.title("RGB histogram for image")
+    plot.xlabel("Color bins")
+    plot.ylabel("# pixels")
+    plot.plot(hist)
+    plot.xlim([0, 255])
+    plot.show()
+
+def getImageHistogram(img):
+    "Returns color histogram"
+    rgb_channel = ['b', 'g', 'r']
+    grayscale = [0]
+    # cv2.calcHist([images], channels, mask, histSize, ranges)
+    hist = cv2.calcHist(
+        [img],          # Images list
+         grayscale,    # Channel. We want RGB
+         None,          # Mask. We dont want to specify a region... yet
+         [256],      # Bins. 256 / 8
+         [0, 256])      # Range of colors
+    plotHistogram(hist)
 
 def computeEuclideanDistance(ind1, ind2, attrLength = 0):
 
@@ -24,12 +54,10 @@ def splitTrainingTestSet(data, trainingProp = 0.8):
     test = data[idxToCut+1:len(data)]
     return training, test
 
-
-def extractImageProperties(ind):
-    img = ind[0]
-    
-    return
-
+def showImage(img):
+    cv2.imshow('',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows() 
 
 def main(argv):
 
@@ -54,12 +82,26 @@ def main(argv):
     print(mat[0][0])
 
     training, test = splitTrainingTestSet(mat, 0.8)
-    print (len(training))
-    print (len(test))
+    # print (len(training))
+    # print (len(test))
+
     for ind in training:
-        extractImageProperties(ind)
+        image_path = os.path.join(_PATH_TO_PHOTOS, ind[0])
+        if os.path.exists(_PATH_TO_PHOTOS):
+            img = cv2.imread(image_path, 0)
 
+            showImage(img)
 
+            ind.append(getImageFeatures(img))
+            ind.append(getImageHistogram(img))
+            break
+        else:
+            print("Path does not exist" + str(image_path))
+
+    # ind[0]: image path
+    # ind[1]: Label 
+    # ind[2]: image properties
+    # ind[3]: img histogram in RGB
 
 if __name__ == "__main__":
    main(sys.argv[1:])
