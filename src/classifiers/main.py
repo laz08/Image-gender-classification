797b1f03b5a2ed79a84294fa2a64962384ae6d6a
Carles Garriga <carles.garriga@est.fib.upc.edu>
2018-06-15 12:33:47 +0200

    fixed indentation + forced one method only

--- src/classifiers/main.py
+++ src/classifiers/main.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import sys, getopt, os, time
+import sys
+import getopt
+import time
 
 import Utils
 import ClassifierManager as cm
 
-PERFORM_KNN = True
+PERFORM_KNN = False
 PERFORM_SVM = False
 PERFORM_MLP = False
 PERFORM_RAND_FOREST = False
+
+PERFORM_KNN = PERFORM_KNN and not PERFORM_SVM and not PERFORM_MLP and not PERFORM_RAND_FOREST
+PERFORM_SVM = PERFORM_SVM and not PERFORM_KNN and not PERFORM_MLP and not PERFORM_RAND_FOREST
+PERFORM_MLP = PERFORM_MLP and not PERFORM_SVM and not PERFORM_KNN and not PERFORM_RAND_FOREST
+PERFORM_RAND_FOREST = PERFORM_RAND_FOREST and not PERFORM_KNN and not PERFORM_SVM and not PERFORM_MLP
+
 
 def usage():
 
@@ -19,23 +27,21 @@ def usage():
 
 def main(argv):
 
-    
     labels = []
     if len(argv) < 1:
          usage()
     try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
     except getopt.GetoptError:
         usage()
 
     for opt, arg in opts:
         if opt == '-h':
-    	        usage()
+            usage()
         elif opt in ("-i", "--ifile"):
             filein = arg
         elif opt in ("-s", "--source"):
             filein = arg
-
 
     print ("Reading from file " + filein)
     mat = Utils.readAsMatrix(filein)
@@ -50,61 +56,59 @@ def main(argv):
     training, test = Utils.splitTrainingTestSet(mat)
 
     # train a Linear SVM on the data
-    if(PERFORM_KNN):
-    	
-    	startTime = time.time()
-    	
-    	acc = cm.performKNeighbors(training, test)
-    	print("Accuracy with Knn: {}%".format(acc))
-    	
-    	endTime = time.time()
-    	elapsedTime = endTime - startTime
-    	timeAsStr = time.strftime("%M:%S", time.gmtime(elapsedTime))
-    	print("Elapsed time: {}".format(timeAsStr))
+    if PERFORM_KNN:
 
-    
-    if(PERFORM_SVM):
-    	
-    	startTime = time.time()
-    	
-    	acc = cm.performLinearSVC(training, test, mat)
-    	print("SVM accuracy: {}%".format(acc))
+        startTime = time.time()
 
-    	endTime = time.time()
-    	elapsedTime = endTime - startTime
-    	timeAsStr = time.strftime("%M:%S", time.gmtime(elapsedTime))
-    	print("Elapsed time: {}".format(timeAsStr))
+        acc = cm.performKNeighbors(training, test)
+        print("Accuracy with Knn: {}%".format(acc))
 
-    
-    if(PERFORM_MLP):
+        endTime = time.time()
+        elapsedTime = endTime - startTime
+        timeAsStr = time.strftime("%M:%S", time.gmtime(elapsedTime))
+        print("Elapsed time: {}".format(timeAsStr))
 
-    	startTime = time.time()
-    	
-    	acc = cm.performMLPClassifier(training, test)
-    	print("MLP accuracy: {}%".format(acc))
+    if PERFORM_SVM:
 
-    	endTime = time.time()
-    	elapsedTime = endTime - startTime
-    	timeAsStr = time.strftime("%M:%S", time.gmtime(elapsedTime))
-    	print("Elapsed time: {}".format(timeAsStr))
+        startTime = time.time()
 
-    
-    if(PERFORM_RAND_FOREST):
+        acc = cm.performLinearSVC(training, test, mat)
+        print("SVM accuracy: {}%".format(acc))
 
-    	startTime = time.time()
+        endTime = time.time()
+        elapsedTime = endTime - startTime
+        timeAsStr = time.strftime("%M:%S", time.gmtime(elapsedTime))
+        print("Elapsed time: {}".format(timeAsStr))
 
-    	acc = cm.performDecisionTreeClassifier(training, test)
-    	print("Rand. forest accuracy: {}%".format(acc))
+    if PERFORM_MLP:
 
-    	endTime = time.time()
-    	elapsedTime = endTime - startTime
-    	timeAsStr = time.strftime("%M:%S", time.gmtime(elapsedTime))
-    	print("Elapsed time: {}".format(timeAsStr))
+        startTime = time.time()
+
+        acc = cm.performMLPClassifier(training, test)
+        print("MLP accuracy: {}%".format(acc))
+
+        endTime = time.time()
+        elapsedTime = endTime - startTime
+        timeAsStr = time.strftime("%M:%S", time.gmtime(elapsedTime))
+        print("Elapsed time: {}".format(timeAsStr))
+
+    if PERFORM_RAND_FOREST:
+
+        startTime = time.time()
+
+        acc = cm.performDecisionTreeClassifier(training, test)
+        print("Rand. forest accuracy: {}%".format(acc))
+
+        endTime = time.time()
+        elapsedTime = endTime - startTime
+        timeAsStr = time.strftime("%M:%S", time.gmtime(elapsedTime))
+        print("Elapsed time: {}".format(timeAsStr))
     
    
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
+
 
 def __init__(self):
     # store the number of points and radius
-	self.numPoints = 24
+    self.numPoints = 24
