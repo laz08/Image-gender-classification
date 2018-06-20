6fe3bfb72b8aa568b52460c574e5fb5d8dc108da
Carles Garriga <carles.garriga@est.fib.upc.edu>
2018-06-20 14:04:43 +0200

    last cnn model

--- attribute_model.py
+++ attribute_model.py
@@ -4,7 +4,7 @@ from keras import backend as K
 from keras import optimizers
 from keras.layers import Input, Dense, Flatten, Activation, Dropout, Reshape
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, merge
-from keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint
+from keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 import keras.metrics
 from keras.models import Sequential, Model
 from keras.utils import np_utils
@@ -16,6 +16,9 @@ from PIL import Image
 import tensorflow as tf
 import csv
 import sys
+from skimage import feature
+import matplotlib.pyplot as plt
+
 
 sys.path.insert(0, 'src/classifiers/')
 import Utils
@@ -58,15 +61,10 @@ def load_images(path, data, ind_gender, train_ratio):
 
     # IMAGES PER INDIVIDUAL
     for i, name in enumerate(data):
-        if count >= MAX_COUNT:
-            break
 
-        # for image_number in range(1, 1):
-        img = cv2.imread(images_path + str(name), 3)
+        img = cv2.imread(images_path + str(name), 0)
         img = Utils.cropToFace(img)
-        img = cv2.resize(img, (WIDTH, HEIGHT))
-        img = np.multiply(img, 1 / 255.0)
-        img = np.asarray(img)
+        # img = cv2.resize(img, (WIDTH, HEIGHT))
 
         if name == previous_name:
             same_instance += 1
@@ -77,36 +75,15 @@ def load_images(path, data, ind_gender, train_ratio):
             count += 1
             same_instance = 0
 
+        feat = feature.local_binary_pattern(img, 24, 3  , method="default")
+        img = np.resize(feat.astype('uint8'), (HEIGHT, WIDTH, CHANNEL))
+        img = np.multiply(img, 1 / 255.0)
+        img = np.asarray(img)
         datas.append(img)
         labels.append(one_hot_encode(ind_gender[i]))
         previous_name = name
-    count  = 0
+    count = 0
     previous_name = ''
-
-    for i, name in reversed(list(enumerate(data))):
-        if count >= MAX_COUNT:
-            break
-
-        # for image_number in range(1, 1):
-        img = cv2.imread(images_path + str(name), 3)
-        img = Utils.cropToFace(img)
-        img = cv2.resize(img, (WIDTH, HEIGHT))
-        img = np.multiply(img, 1 / 255.0)
-        img = np.asarray(img)
-
-        if name == previous_name:
-            same_instance += 1
-
-            if same_instance == 1:
-                continue
-        else:
-            count += 1
-            same_instance = 0
-
-        datas.append(img)
-        labels.append(one_hot_encode(ind_gender[i]))
-        
-        previous_name = name
 
     datas_labels = list(zip(datas, labels))
     random.shuffle(datas_labels)
@@ -141,6 +118,8 @@ def get_cnn_net():
     x = Reshape((1, 2))(x1)
     model = Model(input=inputs, output=x)
 
+    sgd = keras.optimizers.SGD(lr=0.01, momentum=0.9, decay=0.01, nesterov=True)
+
     model.compile(loss='binary_crossentropy', loss_weights=[1.], optimizer='adam', metrics=['accuracy'])
 
     return model
@@ -156,15 +135,15 @@ def load_csv(filename):
     return dataset, labels
 
 
-dataset, labels = load_csv('src/preprocessing/all_labels_old.txt')
+dataset, labels = load_csv('src/preprocessing/merged_labels.txt')
 (train_datas, train_labels, test_datas, test_labels, val_datas, val_labels) = load_images('datasets', dataset, labels, 0.8)
 model = get_cnn_net()
 
 checkpoint = ModelCheckpoint('captcha6_20k_orig_model.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-es= keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.01, patience=2, verbose=1, mode='auto')
-model.fit(train_datas, train_labels, epochs=32, batch_size=32, verbose=1,
-          callbacks=[tensorboard, es, checkpoint], validation_split=0.2, shuffle=True)
-predict_labels = model.predict(test_datas, batch_size=128)
+es = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.01, patience=2, verbose=1, mode='auto')
+hist = model.fit(train_datas, train_labels, epochs=100, batch_size=32, verbose=1,
+          callbacks=[tensorboard, es, checkpoint], validation_split=0.1, shuffle=True)
+predict_labels = model.predict(test_datas, batch_size=32)
 test_size = len(test_labels)
 y1 = test_labels[:, 0, :].argmax(1) == predict_labels[:, 0, :].argmax(1)
 acc = (y1).sum() * 1.0
@@ -172,8 +151,17 @@ acc = (y1).sum() * 1.0
 print('\nmodel evaluate:\nacc: ', acc / test_size)
 print('y1 ', (y1.sum()) * 1.0 / test_size)
 
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+
 # Save model and weights for trained model
-model.save_weights('captcha6_20k_orig_model.h5')
+# model.save_weights('captcha6_20k_orig_model.h5')
 with open('captcha6_20k_orig_model.json', 'w') as f:
     f.write(model.to_json())
 
